f307c6ada31c11fb8ba7afc9367d0fd5f8017bfc
Faukkirik <kote_33419@mai.ru>
2023-01-24 11:04:38 +0300

    2 commit - hw 4

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,14 +27,18 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // задачка на написание онченджа
-        // ЕСЛИ(onChangeChecked ВООБЩЕ СУЩЕСТВУЕТ){
-        //    ПЕРЕДАТЬ ЕМУ ИВЕНТ.ЧТО-ТО.ЧТО-ТО
-        //}
-        // ЕСЛИ(onChange ВООБЩЕ СУЩЕСТВУЕТ){
-        //    ПЕРЕДАТЬ ЕМУ ИВЕНТ
-        //}
-    }
+        if (onChangeChecked){onChangeChecked(e.currentTarget.checked)
+        }
+        if (onChange){onChange(e)}
+
+            // задачка на написание онченджа
+            // ЕСЛИ(onChangeChecked ВООБЩЕ СУЩЕСТВУЕТ){
+            //    ПЕРЕДАТЬ ЕМУ ИВЕНТ.ЧТО-ТО.ЧТО-ТО
+            //}
+            // ЕСЛИ(onChange ВООБЩЕ СУЩЕСТВУЕТ){
+            //    ПЕРЕДАТЬ ЕМУ ИВЕНТ
+            //}
+            }
 
     const finalInputClassName = s.checkbox
         + (className ? ' ' + className : '')
